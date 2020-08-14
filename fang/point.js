342cb7c447db0d4fc50edc3d65795d0c57ca045d
xiaowei_cai <caixiaowei910325@163.com>
2020-08-14 18:05:43 +0800

    hema

--- fang/point.js
+++ fang/point.js
@@ -1,10 +1,10 @@
-function point(map, data, radius=1.2, color='#9966CC') {
-    var layer1 = new Loca.ScatterPointLayer({
+function point(map, data, radius=3, color='#14B4C9') {
+    var layer = new Loca.ScatterPointLayer({
         eventSupport: true,
         map: map
     });
 
-    layer1.on('mousemove', function (ev) {
+    layer.on('mousemove', function (ev) {
         // 事件类型
         var type = ev.type;
         // 当前元素的原始数据
@@ -14,32 +14,41 @@ function point(map, data, radius=1.2, color='#9966CC') {
 
         openInfoWin(map, originalEvent, {
             '名称': rawData.name,
+            '别名': rawData.alias,
+            '挂牌均价': rawData.price,
+            '建筑年代': rawData.year,
+            '建筑类型': rawData.type,
+            '物业费用': rawData.price2,
+            '物业公司': rawData.property,
+            '开发商': rawData.developer,
+            '楼栋总数': rawData.building,
+            '房屋总数': rawData.households,
         });
     });
 
-    layer1.on('mouseleave', function (ev) {
+    layer.on('mouseleave', function (ev) {
         closeInfoWin();
     });
 
     // 传入原始数据
-    layer1.setData(data, {
+    layer.setData(data, {
         lnglat: 'location'
     })
 
     // 配置样式
-    layer1.setOptions({
+    layer.setOptions({
         unit: 'px',
         style: {
-            radius: 1.2,     // 圆形半径，单位像素
-            color: color, // 填充颜色
-            borderWidth: 0.5,   // 边框宽度
-            borderColor: color  // 边框颜色
+            radius: radius,         // 圆形半径，单位像素
+            color: color,           // 填充颜色
+            borderWidth: 0.5,       // 边框宽度
+            borderColor: color      // 边框颜色
         },
         selectStyle: {
-            radius: 14,
-            color: '#FFF684'
+            radius: radius,
+            color: '#14B4C9'
         }
     });
 
-    layer1.render();
+    layer.render();
 }
